7ac3f25a6ab97f56e191fd5b7a7d23e59f235452
mygudou <mygudou@126.com>
2025-01-01 11:26:15 +0000

    gpu

--- src/model.py
+++ src/model.py
@@ -1,14 +1,12 @@
 import torch.nn as nn
 import torch
 
-
 class TimeSeriesTransformer(nn.Module):
     def __init__(self, input_dim, d_model, n_heads, num_layers, dropout=0.1):
         super(TimeSeriesTransformer, self).__init__()
         self.embedding = nn.Linear(input_dim, d_model)
         self.positional_encoding = nn.Parameter(torch.zeros(1, 1000, d_model))
-        self.transformer = nn.Transformer(d_model=d_model, nhead=n_heads, num_encoder_layers=num_layers,
-                                          dropout=dropout)
+        self.transformer = nn.Transformer(d_model=d_model, nhead=n_heads, num_encoder_layers=num_layers, dropout=dropout)
         self.fc_out = nn.Linear(d_model, 1)
 
     def forward(self, x):
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,22 +2,16 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
-
-
 def load_and_preprocess_data(db_handler, window_size):
-    # 从 MongoDB 加载数据
     data = db_handler.fetch_data()
     data['Date'] = pd.to_datetime(data['Date'])
     data.sort_values(by="Date", inplace=True)
 
-    # 选择收盘价作为目标数据
     prices = data['Close'].values.reshape(-1, 1)
 
-    # 数据归一化
     scaler = MinMaxScaler(feature_range=(0, 1))
     prices_scaled = scaler.fit_transform(prices)
 
-    # 时间窗口划分
     def create_sequences(data, window_size):
         X, y = [], []
         for i in range(len(data) - window_size):
