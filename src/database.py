66b0794f2fcf81d1bec35d349bdcc6bca9144a6a
mygudou <mygudou@126.com>
2025-01-01 10:54:51 +0000

    dot env

--- src/database.py
+++ src/database.py
@@ -1,9 +1,18 @@
 from pymongo import MongoClient
 import pandas as pd
+from dotenv import load_dotenv
+import os
 
+# 加载 .env 文件中的环境变量
+load_dotenv()
 
 class MongoDBHandler:
-    def __init__(self, db_name="finance", collection_name="shanghai_composite", uri="mongodb://localhost:27017/"):
+    def __init__(self):
+        # 从环境变量中读取数据库配置
+        uri = os.getenv("MONGODB_URI", "mongodb://localhost:27017/")
+        db_name = os.getenv("DB_NAME", "finance")
+        collection_name = os.getenv("COLLECTION_NAME", "shanghai_composite")
+
         self.client = MongoClient(uri)
         self.db = self.client[db_name]
         self.collection = self.db[collection_name]
